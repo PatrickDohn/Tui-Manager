cfde4f9c5a4f053b6d7bf597042ff9206a10725f
Patrick Dohn <dohnpatrick1993@gmail.com>
2026-02-22 21:49:38 -0500

    add date to task detail-WIP

--- ui/details.go
+++ ui/details.go
@@ -3,17 +3,29 @@ package ui
 import (
 	"fmt"
 	"go-tui/db"
+	"time"
 
 	"github.com/rivo/tview"
 )
 
 func CreateTaskDetailForm(state *UIState, task db.Task, onComplete func()) tview.Primitive {
+
 	form := tview.NewForm()
 
 	// Use the struct fields directly
 	form.AddInputField("Title", task.Title, 30, nil, func(text string) {
 		task.Title = text
 	})
+
+	form.AddInputField("Due Date", task.DueDate.Format("01-02-2006"), 30, nil, func(text string) {
+		t, err := time.Parse("01-02-2006", text)
+		if err != nil {
+			fmt.Println("Error parsing date:", err)
+			return
+		}
+		task.DueDate = t
+	})
+
 	form.AddTextArea("Description", task.Desc, 60, 30, 0, func(text string) {
 		task.Desc = text
 	})
